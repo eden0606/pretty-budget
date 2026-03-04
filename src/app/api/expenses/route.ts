43bdf63542862805a7f70f58c964ea7de26227ed
Eden Chou <eden.chou0606@gmail.com>
2026-03-03 20:33:32 -0500

    updating sql timezone query

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -73,37 +73,34 @@ export async function GET(request: NextRequest) {
           data = await sql`
                 SELECT category, ROUND(CAST(SUM(amount) AS NUMERIC), 2) as total
                 FROM expenses
-                WHERE CAST(date AT TIME ZONE ${timezone} AS TEXT) LIKE ${'%' + match + '%'} 
+                WHERE CAST(date AT TIME ZONE ${timezone} as date AS TEXT) LIKE ${'%' + match + '%'} 
                 GROUP BY category
                 UNION ALL
                 SELECT 'yearly_spend' as category, ROUND(CAST(SUM(amount) AS NUMERIC), 2) as total
                 FROM expenses
-                WHERE CAST(date AT TIME ZONE ${timezone} AS TEXT) LIKE ${'%' + year + '%'} 
+                WHERE CAST(date AT TIME ZONE ${timezone} as date AS TEXT) LIKE ${'%' + year + '%'} 
                 UNION ALL
                 SELECT 'monthly_spend' as category, ROUND(CAST(SUM(amount) AS NUMERIC), 2) as total
                 FROM expenses
-                WHERE CAST(date AT TIME ZONE ${timezone} AS TEXT) LIKE ${'%' + match + '%'} 
+                WHERE CAST(date AT TIME ZONE ${timezone} as date AS TEXT) LIKE ${'%' + match + '%'} 
                 UNION ALL
                 SELECT 'daily_spend' as category, SUM(amount) as total
-                FROM expenses WHERE CAST(date AT TIME ZONE ${timezone} AS TEXT) LIKE ${
+                FROM expenses WHERE CAST(date AT TIME ZONE ${timezone} as date AS TEXT) LIKE ${
             '%' + fullDateMatch + '%'
           } 
                 UNION ALL
                 SELECT 'bilt_spend' as category, SUM(amount) AS total
                 FROM expenses
                 WHERE card = 'wells fargo - bilt - 4376'
-                AND date AT TIME ZONE ${timezone} BETWEEN ${startDate} AND ${endDate}
+                AND date AT TIME ZONE ${timezone} as date BETWEEN ${startDate} AND ${endDate}
                 `;
         } else if (!action && filter === 'none') {
           if (order === 'ASC') {
             data = await sql`
-            SELECT *, date AT TIME ZONE ${timezone} FROM expenses ORDER BY date ASC, id DESC`;
+            SELECT *, date AT TIME ZONE ${timezone} as date FROM expenses ORDER BY date ASC, id DESC`;
           } else if (order === 'DESC') {
             data = await sql`
-            SELECT *, date AT TIME ZONE ${timezone} FROM expenses ORDER BY date DESC, id DESC`;
-          } else {
-            data = await sql`
-            SELECT *, date AT TIME ZONE ${timezone} FROM expenses ORDER BY date, id DESC`;
+            SELECT *, date AT TIME ZONE ${timezone} as date FROM expenses ORDER BY date DESC, id DESC`;
           }
         }
         break;
